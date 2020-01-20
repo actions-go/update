70d7f130f8d905985b35f54ec47031f028a45051
Thibault Jamet <tjamet@users.noreply.github.com>
2020-01-20 20:25:14 +0100

    Adapt to github artifacts

--- main.go
+++ main.go
@@ -44,18 +44,18 @@ func basicAuth(token string) string {
 	return fmt.Sprintf("AUTHORIZATION: basic %s", token)
 }
 
-func setupCredentials() func() error {
+func setupCredentials(repo string) func() error {
 	return func() error {
 		placeHolder := uuid.New().String()
 
 		// Configure a placeholder value. This approach avoids the credential being captured
 		// by process creation audit events, which are commonly logged. For more information,
 		// refer to https://docs.microsoft.com/en-us/windows-server/identity/ad-ds/manage/component-updates/command-line-process-auditing
-		err := git("config", authConfigKey, basicAuth(placeHolder))()
+		err := git("-C", repo, "config", authConfigKey, basicAuth(placeHolder))()
 		if err != nil {
 			return err
 		}
-		fd, err := os.OpenFile(".git/config", os.O_RDWR, 0)
+		fd, err := os.OpenFile(filepath.Join(repo, ".git/config"), os.O_RDWR, 0)
 		if err != nil {
 			return err
 		}
@@ -68,14 +68,9 @@ func setupCredentials() func() error {
 		if !ok {
 			return fmt.Errorf("missing token input")
 		}
-		b := bytes.NewBuffer(nil)
-		_, err = base64.NewEncoder(base64.StdEncoding, b).Write([]byte(fmt.Sprintf("x-access-token:%s", token)))
-		if err != nil {
-			return err
-		}
-		authHeader := b.String()
+		authHeader := base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(fmt.Sprintf("x-access-token:%s", token)))
 		core.SetSecret(authHeader)
-		b = bytes.NewBuffer(nil)
+		b := bytes.NewBuffer(nil)
 		_, err = io.Copy(b, fd)
 		if err != nil {
 			return err
@@ -85,8 +80,10 @@ func setupCredentials() func() error {
 			return err
 		}
 		written, err := fd.Write([]byte(strings.ReplaceAll(b.String(), basicAuth(placeHolder), basicAuth(authHeader))))
-		fd.Truncate(int64(written))
-		return err
+		if err != nil {
+			return err
+		}
+		return fd.Truncate(int64(written))
 	}
 }
 
@@ -94,7 +91,7 @@ func rename() ([]string, error) {
 	renames := map[string]string{
 		"main-windows-latest": "main_windows.exe",
 		"main-macos-latest":   "main_darwin",
-		"main-linux-latest":   "main_linux",
+		"main-ubuntu-latest":  "main_linux",
 	}
 	targets := []string{}
 	err := os.MkdirAll("dist", 0755)
@@ -102,38 +99,43 @@ func rename() ([]string, error) {
 		return []string{}, err
 	}
 	for k, v := range renames {
+		k = filepath.Join(k, "main")
 		dest := filepath.Join("dist", v)
 		err := os.Rename(k, dest)
 		if err != nil {
 			core.Warning(fmt.Sprintf("unable to rename expected file %s: %v", k, err))
-		} else {
-			targets = append(targets, dest)
+			continue
+		}
+		targets = append(targets, dest)
+		err = os.Chmod(dest, 0755)
+		if err != nil {
+			core.Warning(fmt.Sprintf("unable to change permissions to file %s: %v", k, err))
 		}
 	}
 
 	return targets, nil
 }
 
+func gitAdd() error {
+	r, err := rename()
+	if err != nil {
+		return err
+	}
+	return git("add", r...)()
+}
+
 func runMain() error {
 	if github.Context.Payload.Repository == nil || github.Context.Payload.Repository.CloneURL == nil {
 		return fmt.Errorf("missing repository URL in event")
 	}
-	names, err := rename()
-	if err != nil {
-		return err
-	}
-	if len(names) < 1 {
-		return fmt.Errorf("no file to commit")
-	}
 	branch := strings.TrimPrefix(github.Context.Ref, "refs/heads/")
-
 	return firstError(
 		git("init", "."),
 		git("remote", "add", "origin", *github.Context.Payload.Repository.CloneURL),
-		setupCredentials(),
+		setupCredentials("."),
 		git("fetch", "origin"),
 		git("checkout", branch),
-		git("add", names...),
+		gitAdd,
 		git("config", "user.email", "actions-go@users.noreply.github.com"),
 		git("config", "user.name", "actions-go-bot"),
 		git("commit", "-m", "[auto] Add golang action entrypoints"),
--- main_test.go
+++ main_test.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/actions-go/toolkit/core"
 	"github.com/actions-go/toolkit/github"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
 func writeTestFile(t *testing.T, folder, file string) {
-	assert.NoError(t, ioutil.WriteFile(filepath.Join(folder, file), []byte(file), 0755))
+	p := filepath.Join(folder, file)
+	fmt.Println(p)
+	d := filepath.Dir(p)
+	assert.NoError(t, os.MkdirAll(d, 0755))
+	assert.NoError(t, ioutil.WriteFile(p, []byte(file), 0755))
 }
 
 func gitDir(directory string) *string {
@@ -20,7 +26,33 @@ func gitDir(directory string) *string {
 	return &directory
 }
 
+func TestAuthenticate(t *testing.T) {
+	if _, ok := core.GetInput("token"); !ok {
+		t.Skip("missing test token")
+		t.SkipNow()
+	}
+	wd, _ := os.Getwd()
+	defer os.Chdir(wd)
+
+	testID := uuid.New().String()
+	dir, err := ioutil.TempDir("", "test-workdir-"+testID)
+	assert.NoError(t, err)
+	defer os.RemoveAll(dir)
+	assert.NoError(t, os.Chdir(dir))
+
+	assert.NoError(t, firstError(
+		git("init", dir),
+		git("-C", dir, "remote", "-vvv"),
+		git("-C", dir, "remote", "add", "origin", "https://github.com/actions-go/releaser.git"),
+		setupCredentials(dir),
+		git("-C", dir, "fetch", "origin"),
+	))
+}
+
 func TestRunMain(t *testing.T) {
+	wd, _ := os.Getwd()
+	defer os.Chdir(wd)
+	defer os.Setenv("INPUT_TOKEN", os.Getenv("INPUT_TOKEN"))
 	os.Setenv("INPUT_TOKEN", "some-token")
 	wd, err := os.Getwd()
 	assert.NoError(t, err)
@@ -46,9 +78,12 @@ func TestRunMain(t *testing.T) {
 
 	assert.NoError(t, git("init", testUpstream)())
 	writeTestFile(t, testUpstream, "README.md")
+	writeTestFile(t, testUpstream, "dist/main_linux")
+	writeTestFile(t, testUpstream, "dist/main_windows.exe")
+	writeTestFile(t, testUpstream, "dist/main_darwin")
 	assert.NoError(t, git("-C", testUpstream, "config", "user.email", "actions-go@users.noreply.github.com")())
 	assert.NoError(t, git("-C", testUpstream, "config", "user.name", "actions-go-bot")())
-	assert.NoError(t, git("-C", testUpstream, "add", "README.md")())
+	assert.NoError(t, git("-C", testUpstream, "add", "README.md", "dist")())
 	assert.NoError(t, git("-C", testUpstream, "commit", "-m", "add initial files")())
 
 	assert.NoError(t, git("-C", testUpstream, "branch", releaseBranch)())
@@ -56,11 +91,11 @@ func TestRunMain(t *testing.T) {
 	dir, err := ioutil.TempDir("", "test-workdir-"+testID)
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
-	err = os.Chdir(dir)
+	assert.NoError(t, os.Chdir(dir))
 
-	writeTestFile(t, dir, "main-windows-latest")
-	writeTestFile(t, dir, "main-macos-latest")
-	writeTestFile(t, dir, "main-linux-latest")
+	writeTestFile(t, dir, filepath.Join("main-windows-latest", "main"))
+	writeTestFile(t, dir, filepath.Join("main-macos-latest", "main"))
+	writeTestFile(t, dir, filepath.Join("main-ubuntu-latest", "main"))
 
 	assert.NoError(t, runMain())
 
